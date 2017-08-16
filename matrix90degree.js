dcb47ad4eb78e53829c0a7a6addfe1026769bd0b
Apurva Shastry <apurva.shastry@gmail.com>
2017-08-16 14:03:00 -0700

    basic matrix tilt. array.fill behaves in weird ways.

--- matrix90degree.js
+++ matrix90degree.js
@@ -5,24 +5,26 @@
 // c : in place
 // e : 
 
-// not int place
-const matTranspose = (mat) => {
+// not in place
+const matTilt = (mat) => {
   console.log('args: ', mat);
-  let j2 = mat.length - 1;;
-  let i2;
-  const newMat = new Array(mat[1].length);
-  newMat.fill([]);
+  const newMat = new Array();
+  for (let i = 0; i < mat.length; i++) {
+    newMat.push([0, 0]);
+  }
+  console.log(JSON.stringify(newMat));
+  newMat[0][0] = 'hi';
+  console.log(JSON.stringify(newMat));
   for (let i = 0; i < mat.length; i++) {
     for (let j = 0; j < mat[1].length; j++) {
-      i2 = j;
-      console.log('(i, j) : (', i, ', ', j, ')');
-      console.log('(i2, j2) : (', i2, ', ', j2, ')');
-      
-      newMat[i2][j2] = mat[i][j];
+      console.log('(i, j) -> (i2, j2) : (', i, ', ', j, ') -> (', j, ', ', mat.length - 1 - i, ')');
+      let y2 = mat.length - 1 - i;
+      console.log('mat j : ', newMat[j]);
+      newMat[j][y2] = (mat[i][j]);
+      console.log('new mat: ', JSON.stringify(newMat));
     }
-    j2--;
   }
   return newMat;
 }
 
-console.log(JSON.stringify(matTranspose(new Array( new Array(1, 2), new Array(3, 4)))));
+console.log(JSON.stringify(matTilt(new Array( new Array(1, 2), new Array(3, 4)))));
