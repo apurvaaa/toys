e129680ba53971290fe6c53acd3c4f1b5b3aade9
Apurva Shastry <apurva.shastry@gmail.com>
2017-10-02 03:08:53 -0700

    implemented deep equality

--- deepEquality.js
+++ deepEquality.js
@@ -11,6 +11,38 @@
   * don't worry about handling cyclical object structures.
   *
   */
-  //TODO:
   var deepEquals = function(apple, orange) {
-};
+    const keys1 = Object.keys(apple);
+    const keys2 = Object.keys(orange);
+    if (keys1.length !== keys2.length) {
+      // console.log('1')
+      return false;
+    }
+    for (let key of keys1) {
+      if (orange[key] === undefined) {
+        // console.log('2');
+        return false;
+      }
+      if (typeof apple[key] ===  typeof orange[key]) {
+        if (typeof apple[key] === 'object') {
+          let subSol =  deepEquals(apple[key], orange[key]);
+          if (!subSol) {
+            // console.log('3');
+            return false;
+          }
+        } else {
+          if (apple[key] !== orange[key]) {
+            // console.log('4');
+            return false;
+          }
+        }
+      } else {
+        // console.log('5');
+        return false;
+      }
+    }
+    return true;
+  };
+
+  console.log(deepEquals({a:1, b: {c:3}},{a:1, b: {c:3}})); // true
+  console.log(deepEquals({a:1, b: {c:5}},{a:1, b: {c:6}})); // false
