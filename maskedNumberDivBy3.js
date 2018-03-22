8a6004792ae1058a632490a260f1137246f420d9
Apurva Shastry <apurva.shastry@gmail.com>
2018-03-21 20:36:15 -0700

    still debugging. 4 tests were failing. Made code more efficient and readable

--- maskedNumberDivBy3.js
+++ maskedNumberDivBy3.js
@@ -1,3 +1,4 @@
+//TODO: 4 tests were failing
 /*
 A masked number is a string that consists of digits and one asterisk (*) that should be replaced by exactly one digit. Given a masked number find all the possible options to replace the asterisk with a digit to produce an integer divisible by 3.
 
@@ -8,7 +9,7 @@ isDivisibleBy3(inputString) = ["120", "150", "180"].
 
 */
 
-function isDivisibleBy3(inputString) {
+function isDivisibleBy31(inputString) {
     let found = false;
     let i = 0;
     let result = [];
@@ -18,10 +19,41 @@ function isDivisibleBy3(inputString) {
     console.log('i : ', i)
     for (let j = 0; j < 10; j++) {
         let newRes = inputString.slice(0,i)+ j.toString() + inputString.slice(i + 1)
-        console.log('newRes : ', newRes)
+        // console.log('newRes : ', newRes)
         if (parseInt(newRes) % 3 === 0) {
             result.push(newRes)
         }
     }
     return result;
 }
+
+function isDivisibleBy3(inputString) {
+    if (inputString.legth < 1) return [];
+    const index = inputString.indexOf('*') 
+    if (index === -1) return []
+    let numbers = '0123456789'.split('');
+    let possibleRes = numbers.map((ele) => {
+        return inputString.slice(0,index) + ele + inputString.slice(index + 1);
+    })
+    console.log(possibleRes)
+    let res = possibleRes.filter((ele) => {
+        return (parseInt(ele) % 3) === 0})
+    return res
+}
+
+// tests
+// console.log(isDivisibleBy3('1*0'))
+// (["120", "150", "180"])
+
+// console.log(isDivisibleBy3('*'))
+//[0, 3, 6, 9]
+// console.log(isDivisibleBy3('1234567890*'))
+//["12345678900", "12345678903", "12345678906", "12345678909"]
+
+// console.log(isDivisibleBy3('4*0'))
+//["420", "450", "480"]
+
+// console.log(isDivisibleBy3('1*0'))
+//["120", "150", "180"]
+
+console.log(isDivisibleBy3(''))
