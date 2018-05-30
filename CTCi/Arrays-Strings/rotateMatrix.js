af9b0934868c7f02a54ae787cda3909f2e8370a5
Apurva Shastry <apurva.shastry@gmail.com>
2018-05-29 21:58:51 -0700

    rotate matrix completed

--- CTCi/Arrays-Strings/rotateMatrix.js
+++ CTCi/Arrays-Strings/rotateMatrix.js
@@ -1,4 +1,31 @@
-// Rotate Matrix: Given an image represented by n x n matrix, where each pixel in the image is 4 bytes, write a method to 
+// Rotate Matrix: Given an image represented by n x n matrix, where each pixel in the image is 4 bytes, 
+// write a method to 
 // rotate the image 90 degrees. Can you do it in place?
 
+
+const rotateMatrix = (matrix) => {
+    // works for matrix size 2 or more
+
+    // levels
+    for (let i = 0;  i < Math.floor(matrix.length / 2); i++) {
+        // rows to turn 
+        for (let j = i; j < matrix.length - 1 - i; j++) {
+
+            // let temp = matrix[i][j]
+            // matrix[i][j] = matrix[j][matrix.length - 1 - i] 
+            // matrix[j][matrix.length - 1 - i] = matrix[matrix.length - 1 - i][matrix.length - 1 - j]
+            // matrix[matrix.length - 1 - i][matrix.length - 1 - j] = matrix[matrix.length - 1 - j][i]
+            // matrix[matrix.length - 1 - j][i] = temp
+
+            let temp = matrix[i][j]
+            matrix[i][j] = matrix[matrix.length - 1 - j][i]
+            matrix[matrix.length - 1 - j][i] = matrix[matrix.length - 1 - i][matrix.length - 1 - j]
+            matrix[matrix.length - 1 - i][matrix.length - 1 - j] = matrix[j][matrix.length - 1 - i]
+            matrix[j][matrix.length - 1 - i] = temp
+        }
+    }
+    return matrix
+}
+
+console.log(rotateMatrix([[1, 2],[3, 4]]))
 
